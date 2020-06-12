cb3bc9ea418e7e3ed96caa25b777e41ec8e86229
Robin-1995 <robinplatts@live.co.uk>
2020-06-12 16:43:31 +0100

    fixed whitespace and commented out test cases

--- LRU Cache.py
+++ LRU Cache.py
@@ -1,16 +1,14 @@
 import collections
 
-class LRU_Cache(object):
+class LRUCache(object):
 
     def __init__(self, capacity):
         if not isinstance(capacity, int) or capacity <= 0:
             print(f"Cache capacity of {capacity} is invalid: Capacity must be an integer greater than 0, no Cache created. Function aborting")
             return
-
         self.capacity = capacity
         self.storage = collections.OrderedDict()
         self.num_items = 0
-
 
     def get(self, key):
         '''
@@ -20,18 +18,16 @@ class LRU_Cache(object):
 
         if key in self.storage:
 
-            self.storage.move_to_end(key)   #<-- this key has been used most recently so move to end of the ordereddict
+            self.storage.move_to_end(key)   # <-- this key has been used most recently so move to end of the ordereddict
             return self.storage[key]
 
         return -1
-
 
     def at_capacity(self):
         '''
         :return: True if number of items in dict is equal to the max capacity of the cache, else False
         '''
         return self.num_items == self.capacity
-
 
     def delete_cache_entry(self, key):
         '''
@@ -45,13 +41,11 @@ class LRU_Cache(object):
             self.num_items -= 1
         return
 
-
     def least_recently_used(self):
         '''
         :return: the key of the least recently used item in the cache
         '''
         return next(iter(self.storage))
-
 
     def set(self, key, value):
 
@@ -61,7 +55,7 @@ class LRU_Cache(object):
 
         else:
 
-            if self.at_capacity():  #<-- key not in the cache so we add it, first check if the cache is at capacity
+            if self.at_capacity():  # <-- key not in the cache so we add it, first check if the cache is at capacity
 
                 to_delete = self.least_recently_used()
                 self.delete_cache_entry(to_delete)
@@ -73,40 +67,40 @@ class LRU_Cache(object):
 
         return
 
-# Test Case 1:
-our_cache = LRU_Cache(5)
-
-our_cache.set(1, 1);
-our_cache.set(2, 2);
-our_cache.set(3, 3);
-our_cache.set(4, 4);
-
-
-print(our_cache.get(1))      # returns 1
-print(our_cache.get(2))       # returns 2
-print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
-
-our_cache.set(5, 5)
-our_cache.set(6, 6)
-
-print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
-
-# Test Case 2:
-cache_2 = LRU_Cache(0)       # returns None
-
-# Test Case 3:
-cache_2 = LRU_Cache("Hello")    # returns None
-
-
-# Test Case 4:
-cache_4 = LRU_Cache(1000)
-
-for i in range(0,1001):
-    cache_4.set(i,i)
-
-print(cache_4.get(1001)) # returns -1 since 1001 is not in the cache
-print(cache_4.get(0)) # <-- returns -1 because 0 was the first cache entry would have been the least recently used for the 1001th entry
-print(cache_4.get(100)) #<-- return 100
-print(cache_4.get(1000)) #<-- return 1000
+# # Test Case 1:
+# our_cache = LRU_Cache(5)
+#
+# our_cache.set(1, 1);
+# our_cache.set(2, 2);
+# our_cache.set(3, 3);
+# our_cache.set(4, 4);
+#
+#
+# print(our_cache.get(1))      # returns 1
+# print(our_cache.get(2))       # returns 2
+# print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+#
+# our_cache.set(5, 5)
+# our_cache.set(6, 6)
+#
+# print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+#
+# # Test Case 2:
+# cache_2 = LRU_Cache(0)       # returns None
+#
+# # Test Case 3:
+# cache_2 = LRU_Cache("Hello")    # returns None
+#
+#
+# # Test Case 4:
+# cache_4 = LRU_Cache(1000)
+#
+# for i in range(0,1001):
+#     cache_4.set(i,i)
+#
+# print(cache_4.get(1001)) # returns -1 since 1001 is not in the cache
+# print(cache_4.get(0)) # <-- returns -1 because 0 was the first cache entry would have been the least recently used for the 1001th entry
+# print(cache_4.get(100)) #<-- return 100
+# print(cache_4.get(1000)) #<-- return 1000
 
 
